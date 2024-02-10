588c82ddda2931e9fad662f6bd324cd470f62f87
Divy Gupta <divygupta208@gmail.com>
2024-02-10 15:56:49 +0530

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Header.css";
 import MusicSection from "./MusicSection";
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <div className="header-bg ">
         <div className="header">The Generics</div>
       </div>
-      <MusicSection />
     </>
   );
 };
