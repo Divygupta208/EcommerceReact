bf0bc0b2613cce5898aee2c93e007322510db667
Divy Gupta <divygupta208@gmail.com>
2024-02-14 12:51:05 +0530

    product route

---
+++ src/pages/Product.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductDetails from "../components/ProductDetails";
+
+const Product = () => {
+  return (
+    <div className="product-details">
+      <ProductDetails />
+    </div>
+  );
+};
+
+export default Product;
